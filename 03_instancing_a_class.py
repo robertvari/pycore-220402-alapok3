6e9c6caa23fecfe9c635230226cc183c1679e0ec
Robert Vári <mail.robertvari@gmail.com>
2022-05-07 14:24:54 +0200

    update

---
+++ 03_instancing_a_class.py
@@ -0,0 +1,15 @@
+class Dice:
+    color = "white"
+    sides = 6
+
+
+# instancing a class
+my_dice = Dice()
+print(my_dice.color)
+print(my_dice.sides)
+
+my_dice2 = Dice()
+my_dice2.color = "red"
+my_dice2.sides = 10
+print(my_dice2.color)
+print(my_dice2.sides)
