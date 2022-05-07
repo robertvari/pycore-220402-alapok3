314c002777084fe1e02ecadbca01f97abbb43115
Robert Vári <mail.robertvari@gmail.com>
2022-05-07 14:52:40 +0200

    update

--- 04_constructor.py
+++ 04_constructor.py
@@ -1,16 +1,23 @@
 class Dice:
+    # class attribute
+
     # constructor
     def __init__(self, color, sides):
-        print(f"A dice was born. Color: {color} Sides: {sides}")
-
         # instance attribute
         self.color = color
         self.sides = sides
 
 
-my_dice1 = Dice("white", 6)
-my_dice2 = Dice("red", 10)
-my_dice3 = Dice("blue", 20)
+class Person:
+    race = "human"
 
-print(my_dice1.color, my_dice1.sides)
-print(my_dice2.color, my_dice2.sides)
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+robert = Person("Robert", "Budapest")
+tamas = Person("Tamás", "Pécs")
+Person.race = "animal"
+print(robert.name, robert.address, robert.race)
+print(tamas.name, tamas.address, tamas.race)
