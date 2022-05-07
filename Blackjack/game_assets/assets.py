cb533b124d62ed3454914d83be8e255798e6a23c
Robert Vári <mail.robertvari@gmail.com>
2022-05-07 16:51:34 +0200

    project init

--- Blackjack/game_assets/assets.py
+++ Blackjack/game_assets/assets.py
@@ -1,6 +1,7 @@
+import random
+
 class Card:
     def __init__(self, name, value):
-        print("Card created", name, value)
         self._name = name
         self._value = value
 
@@ -11,6 +12,9 @@ class Card:
     @property
     def value(self):
         return self._value
+
+    def __repr__(self):
+        return f"{self.name}"
 
 
 class Deck:
@@ -36,7 +40,33 @@ class Deck:
 
         names = ["Heart", "Club", "Diamond", "Spade"]
 
+        for name in names:
+            for card in cards:
+                new_card = Card(f"{name} {card[0]}", card[1])
+                self._cards.append(new_card)
+
+        # shuffle deck
+        random.shuffle(self._cards)
+
+    @property
+    def cards(self):
+        return self._cards
+
+    def draw(self):
+        new_card = self._cards[0]
+        self._cards.remove(new_card)
+        return new_card
 
 if __name__ == "__main__":
     deck = Deck()
     deck.create()
+    print(deck.cards)
+
+    # drew few cards from deck
+    card1 = deck.draw()
+    card2 = deck.draw()
+    card3 = deck.draw()
+
+    print(card1, card2, card3)
+
+    print(deck.cards)
