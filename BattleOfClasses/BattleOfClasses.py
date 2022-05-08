81c80d9aabff3b6dd3e263dfc137ded8d51ab71e
Robert Vári <mail.robertvari@gmail.com>
2022-05-08 14:17:55 +0200

    init

--- BattleOfClasses/BattleOfClasses.py
+++ BattleOfClasses/BattleOfClasses.py
@@ -1,5 +1,18 @@
+import os
+
+
 class BattleOfClasses:
-    pass
+    def __init__(self):
+        self._intro()
+
+    @staticmethod
+    def clear_screen():
+        os.system("cls" if os.name == "nt" else "clear")
+
+    def _intro(self):
+        self.clear_screen()
+        print("-"*50, "BATTLE OF CLASSES", "-"*50)
 
 
 BattleOfClasses()
+
