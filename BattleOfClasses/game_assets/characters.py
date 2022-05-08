29988ba93bf7c21057ff68286a71ee1feba0b4f5
Robert Vári <mail.robertvari@gmail.com>
2022-05-08 14:35:50 +0200

    init

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,4 +1,11 @@
 class CharacterBase:
+    races = {
+        "human": {"strength": 50, "max_HP": 100, "max_weight": 50},
+        "ork": {"strength": 130, "max_HP": 200, "max_weight": 100},
+        "elf": {"strength": 60, "max_HP": 100, "max_weight": 60},
+        "dwarf": {"strength": 130, "max_HP": 230, "max_weight": 150},
+    }
+
     def __init__(self):
         pass
 
